37ff9f55fd7992f6b057c901208ca932019d3abb
slartus <slartus@gmail.com>
2022-02-28 10:05:46 +0500

    tmdb popular page selection

--- common/src/commonMain/kotlin/ru/slartus/moca/data/tmdb/TmdbApi.kt
+++ common/src/commonMain/kotlin/ru/slartus/moca/data/tmdb/TmdbApi.kt
@@ -6,6 +6,7 @@ import ru.slartus.moca.data.tmdb.models.Genre
 import ru.slartus.moca.data.tmdb.models.GenresResponse
 import ru.slartus.moca.domain.models.Movie as RepositoryMovie
 import ru.slartus.moca.data.tmdb.models.PopularResponse
+import kotlin.jvm.JvmInline
 
 class TmdbApi(val client: HttpClient) {
     inner class Genres {
@@ -17,9 +18,9 @@ class TmdbApi(val client: HttpClient) {
     }
 
     inner class Movies {
-        suspend fun getPopular(): List<RepositoryMovie> {
+        suspend fun getPopular(page: MoviesPage = MoviesPage(1)): List<RepositoryMovie> {
             val genresResponse: PopularResponse =
-                client.get("$END_POINT/movie/popular?$DEFAULT_PARAMS")
+                client.get("$END_POINT/movie/popular?page=${page.page}&$DEFAULT_PARAMS")
             return (genresResponse.results ?: emptyList()).mapNotNull {
                 val id = it.id ?: return@mapNotNull null
                 return@mapNotNull RepositoryMovie(
@@ -35,5 +36,12 @@ class TmdbApi(val client: HttpClient) {
         private const val API_KEY = "9c97850f98d684bace19186d2979504f"
         private const val LANGUAGE = "ru-RU"
         private const val DEFAULT_PARAMS = "language=$LANGUAGE&api_key=$API_KEY"
+    }
+}
+
+@JvmInline
+value class MoviesPage(val page: Int) {
+    init {
+        require(page in 1..1000)
     }
 }
