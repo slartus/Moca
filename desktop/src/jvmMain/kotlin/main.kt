1d58da95305b2ffc66bf934bf4e0cf5276a2d3dd
slartus <slartus@gmail.com>
2022-03-01 20:31:02 +0500

    desktop window size

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -2,10 +2,12 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import ru.slartus.moca.core_ui.PlatformSettings
 import ru.slartus.moca.core_ui.theme.AppTheme
+import java.awt.Dimension
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         this.window.title = "Moca"
+        this.window.size = Dimension(1024, 840)
         val platformSettings = PlatformSettings(getPlatform())
         AppTheme(platformSettings = platformSettings, darkTheme = true) {
             App()
