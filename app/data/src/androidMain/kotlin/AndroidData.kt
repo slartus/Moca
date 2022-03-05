d83f0becf4831edde4335ec66402ec8040341086
slartus <slartus@gmail.com>
2022-03-05 17:59:36 +0500

    sqldelight

--- app/data/src/androidMain/kotlin/AndroidData.kt
+++ app/data/src/androidMain/kotlin/AndroidData.kt
@@ -1,3 +1,6 @@
+import android.content.Context
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
 import io.github.aakira.napier.Napier
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -5,8 +8,15 @@ import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
+import ru.slartus.moca.db.MocaDatabase
 
 actual fun getHttpClient() = DataAppResolve.getHttpClient()
+
+actual class SqlDelightDriverFactory(private val context: Context) {
+    actual fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(MocaDatabase.Schema, context, "moca.db")
+    }
+}
 
 object DataAppResolve {
     fun getHttpClient(): HttpClient = HttpClient(CIO) {
