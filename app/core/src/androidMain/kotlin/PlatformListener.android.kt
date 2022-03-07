6c91607dfb29f7d60d05dc61879303b8ad1084d3
slartus <slartus@gmail.com>
2022-03-07 16:35:18 +0500

    desktop open url

--- app/core/src/androidMain/kotlin/PlatformListener.android.kt
+++ app/core/src/androidMain/kotlin/PlatformListener.android.kt
@@ -3,9 +3,12 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
+import java.lang.ref.WeakReference
 
 
-actual class PlatformListener(val context: Context) {
+actual class PlatformListener(context: Context) {
+    private val contextWeakRef = WeakReference(context)
+    private val context get() = contextWeakRef.get()!!
     actual fun openUrl(url: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
--- app/core/src/desktopMain/kotlin/PlatformListener.desktop.kt
+++ app/core/src/desktopMain/kotlin/PlatformListener.desktop.kt
@@ -1,4 +1,17 @@
+import java.awt.Desktop
+import java.net.URI
+import java.net.URISyntaxException
+
+
 actual class PlatformListener {
     actual fun openUrl(url: String) {
+        val desktop: Desktop = java.awt.Desktop.getDesktop()
+        try {
+            val oURL = URI(url)
+            desktop.browse(oURL)
+        } catch (e: URISyntaxException) {
+            e.printStackTrace()
+        }
+
     }
 }
