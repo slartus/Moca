00261bfeb77e74f3aab92ffcadffca69ae786498
slartus <slartus@gmail.com>
2022-03-19 11:37:47 +0500

    desktop package name

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -34,7 +34,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "KotlinMultiplatformComposeDesktopApplication"
+            packageName = "Moca"
             packageVersion = "1.0.0"
         }
     }
