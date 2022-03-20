73dbb283f83c8f28399055258e984ce711e77ef9
slartus <slartus@gmail.com>
2022-03-20 14:19:55 +0500

    external annotation removed

--- app/feature-main/src/commonMain/kotlin/ru/slartus/moca/features/feature-main/videoGridViews/ProductsView.kt
+++ app/feature-main/src/commonMain/kotlin/ru/slartus/moca/features/feature-main/videoGridViews/ProductsView.kt
@@ -1,16 +1,17 @@
 package ru.slartus.moca.features.`feature-main`.videoGridViews
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.kodein.di.compose.rememberInstance
-import ru.slartus.moca.`core-ui`.views.VideoCard
 import ru.slartus.moca.`core-ui`.theme.AppTheme
+import ru.slartus.moca.`core-ui`.views.VideoCard
 import ru.slartus.moca.domain.models.Movie
 import ru.slartus.moca.domain.models.Product
 import ru.slartus.moca.domain.models.Series
@@ -51,7 +52,6 @@ internal fun SeriesView(
     )
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun <T : Product> ProductsView(
     modifier: Modifier = Modifier,
